1747253452b783dc0a1b19748baf584641ca5702
Luke van der Hoeven <hi@plukevdh.me>
2016-05-31 17:35:17 -0400

    add shell commands

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,41 @@
-import cli from 'commander'
-import
+import versionParser from './utils/versionParser'
+import program from 'commander'
 
-cli
-  .version('0.0.1')
+function setupCLI(cli) {
+  cli.usage('[options] <config file>');
+  return cli;
+}
+
+function addConfigCommand(cli) {
+  cli.command('config');
+  return cli;
+}
+
+function addJwtCommand(cli) {
+  cli.command('jwt');
+  return cli;
+}
+
+function addDumpCommand(cli) {
+  cli.command('dump');
+  return cli;
+}
+
+function addApplyCommand(cli) {
+  cli.command('apply');
+  return cli;
+}
+
+export default function cli(args) {
+  const cli = program;
+
+  versionParser()
+    .then(version => cli.version(version))
+    .then(setupCLI)
+    .then(addConfigCommand)
+    .then(addJwtCommand)
+    .then(addDumpCommand)
+    .then(addApplyCommand)
+    .then(cli => cli.parse(args))
+    .catch(console.error);
+}

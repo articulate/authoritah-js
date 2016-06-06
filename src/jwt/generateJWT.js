7b61d06090ac7f833108cf2b3903e30a962fda24
Luke van der Hoeven <hi@plukevdh.me>
2016-06-06 17:27:00 -0400

    cleanup jwt generation
    no need for promising here

--- src/jwt/generateJWT.js
+++ src/jwt/generateJWT.js
@@ -1,6 +1,5 @@
+import crypto from 'crypto'
 import jwt from 'jsonwebtoken'
-
-import promisify from '../utils/promisify'
 
 const DEFAULT_SCOPE = {
   rules: {
@@ -14,13 +13,28 @@ const DEFAULT_SCOPE = {
 };
 
 export default function generateJWT(context) {
-  const { uid, created_at, key, secret } = context;
-  const decodedSecret = Buffer.from(secret, 'base64');
+  const {
+    config,
+    options: {
+      secret,
+      key,
+      refresh,
+      }
+    } = context;
+
+  const apiKey = key || config.orGet('auth0.key', key);
+  const apiSecret = secret || config.orGet('auth0.secret', secret);
+
+  if(refresh) { config.remove('jwt.createdAt', 'jwt.uid'); }
+
+  const createdAt = config.getset('jwt.createdAt', Date.now());
+  const uid = config.getset('jwt.uid', crypto.randomBytes(16).toString('hex'));
+  const decodedSecret = Buffer.from(apiSecret, 'base64');
 
   return jwt.sign({
-                    aud: key,
+                    aud: apiKey,
                     scopes: DEFAULT_SCOPE,
-                    iat: created_at,
+                    iat: createdAt,
                     jti: uid,
                   }, decodedSecret);
 }
--- test/jwt/generateJWTSpec.js
+++ test/jwt/generateJWTSpec.js
@@ -1,22 +1,35 @@
+import loadEnv from '../../src/utils/loadEnv'
 import generateJWT from '../../src/jwt/generateJWT'
+import configManager from '../../src/config/configManager'
 
-const managedContext = {
-  key: '123',
-  secret: '456',
-  uid: '567',
-  created_at: 1464799343600
+const config = configManager('./test/fixtures/jwt.json');
+
+const ctx = {
+  config,
+  options: {
+    key: '123',
+    secret: '456'
+  }
 };
 
-const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMjMiLCJzY29wZXMiOnsicnVsZXMiOnsiYWN0aW9ucyI6WyJyZWFkIiwidXBkYXRlIiwiZGVsZXRlIiwiY3JlYXRlIl19fSwiaWF0IjoxNDY0Nzk5MzQzNjAwLCJqdGkiOiI1NjcifQ.XqgX1IHKnTzCTLy8K4z5S7RC_AYPbQA8BFMrkg3At5c';
+const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMjMiLCJzY29wZXMiOnsicnVsZXMiOnsiYWN0aW9ucyI6WyJyZWFkIiwidXBkYXRlIiwiZGVsZXRlIiwiY3JlYXRlIl19fSwiaWF0IjoxNDY1MjI4MjMwMDM2LCJqdGkiOiIxMjMifQ._Fg1RCxrLrMula3DvXZOl4Puq4IF5qRLqSDa_dFjmx8';
 
-describe("generateWT", () => {
-  const result = generateJWT(managedContext);
+describe("generateJWT", () => {
+  beforeEach(() => {
+    config.set('jwt.createdAt', 1465228230036);
+    config.set('jwt.uid', '123');
+  });
 
   it('can generate a repeatable JWT', () => {
-    expect(result).to.equal(jwt);
+    const result = generateJWT(ctx);
+    return expect(result).to.equal(jwt);
   });
 
   it('can generate a fresh JWT', () => {
-    expect(generateJWT({ key: '123', secret: '456' })).not.to.equal(jwt);
+    ctx.options.refresh = true;
+    const result = generateJWT(ctx);
+
+    expect(result).not.to.equal(jwt);
+    expect(config.get('jwt.uid')).not.to.eq('123');
   });
 });

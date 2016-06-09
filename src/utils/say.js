b1a2c1b2feb964cd0e0ad0192c3013c5995bcc85
Luke van der Hoeven <hi@plukevdh.me>
2016-06-09 11:07:24 -0400

    make say more useful

--- src/utils/say.js
+++ src/utils/say.js
@@ -1,25 +1,27 @@
-import { map, always, compose, ifElse, pathOr } from 'ramda'
+import { map, always, compose, ifElse, pathOr, identity, is, join } from 'ramda'
 import cl from 'cli-color'
+import { inspect } from 'util'
 
 const printers = {
   error: [ console.error, cl.red.bold ],
-  warn: [ console.warn, cl.yellow.italics ],
+  warn: [ console.warn, cl.yellow.italic ],
   ok: [ console.log, cl.green ],
   notice: [ console.log, cl.cyan ],
   say: [ console.log ],
 };
 
+const colorlessInspector = (obj) => inspect(obj, { colors: false });
+const doInspect = ifElse(is(String), identity, colorlessInspector);
+const hydrate = (...msg) => compose(join(''), map(doInspect))(msg);
+
 export default function say(options) {
   const color = pathOr(true, ['parent', 'color'], options);
 
-  return map(defn => {
-    let [printer, fmt] = defn;
-    printer = printer.bind(console);
+  return map(([printer, fmt]) => {
+    const boundPrint = printer.bind(console);
 
-    if(!fmt) { return printer; }
+    if(!fmt) { return boundPrint; }
 
-    return ifElse(always(color),
-                  compose(printer, fmt),
-                  printer);
+    return color ? compose(boundPrint, fmt, hydrate) : boundPrint;
   }, printers);
 }

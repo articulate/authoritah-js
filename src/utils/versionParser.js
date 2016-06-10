08f602b7527190c22bb2cff90bd85bde8ee1fa99
Luke van der Hoeven <hi@plukevdh.me>
2016-06-10 16:21:09 -0400

    fix version lookup

--- src/utils/versionParser.js
+++ src/utils/versionParser.js
@@ -3,7 +3,7 @@ import promisify from './promisify'
 import { path } from 'ramda'
 
 export default function versionParser() {
-  return promisify(fs.readFile)('./package.json')
+  return promisify(fs.readFile)(__dirname + '/../../package.json')
     .then(JSON.parse)
     .then(path(['version']))
 }

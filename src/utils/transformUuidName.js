8a119dd70530bea55c7a1976624854cc06ffa12f
Luke van der Hoeven <hi@plukevdh.me>
2016-06-28 17:38:25 -0400

    better edge-case handling of uuids

--- src/utils/transformUuidName.js
+++ src/utils/transformUuidName.js
@@ -2,13 +2,19 @@ import R from 'ramda'
 import idgen from 'idgen'
 
 // includes legacy UUID format matcher
-const uuidMatcher = /([\w]{8}(?:-[\w]{4}){3}-[\w]{12}|[\w-_]{16})?[\s|-]?(.+)/
+const uuidMatcher = /^([\w]{8}(?:-[\w]{4}){3}-[\w]{12}|[\w-_]{16})[ -](.+)$/;
+
+// ensure no underscores as Auth0 has some limitations on name conventions
+const sanitizedId = (...args) => idgen(args).replace(/_/g, '-');
 
 export function extractUuid(attrs) {
   const { name } = attrs;
   let [_original, uuid, namePart] = R.match(uuidMatcher, name);
 
-  if(R.isNil(uuid)) { uuid = idgen(16); }
+  if(R.isNil(uuid)) {
+    uuid = sanitizedId(16);
+    namePart = name;
+  }
 
   return R.merge(attrs, { uuid, name: namePart });
 }
@@ -16,7 +22,7 @@ export function extractUuid(attrs) {
 export function combineUuid(attrs) {
   let { name, uuid } = attrs;
 
-  if(R.isNil(uuid)) { uuid = idgen(16); }
+  if(R.isNil(uuid)) { uuid = sanitizedId(16); }
 
   return R.compose(R.assoc('name', `${uuid} ${name}`), R.dissoc('uuid'))(attrs);
 }
--- test/utils/transformUuidNameSpec.js
+++ test/utils/transformUuidNameSpec.js
@@ -2,10 +2,16 @@ import { extractUuid, combineUuid } from '../../src/utils/transformUuidName'
 
 describe("transformations on UUID name", () => {
   context('extraction', () => {
-    it('can extract uuid from a combined uuid name', () => {
+    it('can extract uuid from a combined uuid name (legacy)', () => {
       const result = extractUuid({name: "EBF20416-414D-42F8-8766-3524F944C28F Name With Face"});
 
       expect(result).to.eql({name: "Name With Face", uuid: "EBF20416-414D-42F8-8766-3524F944C28F" });
+    });
+
+    it('can extract a 16-digit uuid from a combined id name', () => {
+      const result = extractUuid({name: "i_X7ZQWutF6evjKl-my-new-name"});
+
+      expect(result).to.eql({name: "my-new-name", uuid: "i_X7ZQWutF6evjKl" });
     });
 
     it('creates UUID if no UUID found', () => {
@@ -26,7 +32,7 @@ describe("transformations on UUID name", () => {
     it('can generate a uuid if none given', () => {
       const result = combineUuid({name: "Hello World"});
 
-      expect(result.name).to.match(/\w{16} Hello World/);
+      expect(result.name).to.match(/[\w_-]{16} Hello World/);
     });
   });
 });

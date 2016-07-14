b1e91727f866a27f7f2dc4b97787fc1f8b4bffad
Luke van der Hoeven <hi@plukevdh.me>
2016-07-14 13:00:53 -0400

    wrap update connection call

--- src/auth0/connections/updateConnections.js
+++ src/auth0/connections/updateConnections.js
@@ -1,11 +1,13 @@
 import R from 'ramda'
 import prepareUpdate from '../../transformers/connections/prepareConnectionForUpdate'
+import apiCallWrapper from '../../utils/apiCallWrapper'
 
 export default function updateConnections(context) {
   const { client, diff: { connections: { changes } }, say: { notice } } = context;
+  const updateFn = apiCallWrapper(client.connections.update, context);
 
   const print = ({name}) => notice('Updated connection ', name);
-  const updateWrapper = (connection) => client.connections.update({ id: connection.id }, prepareUpdate(connection));
+  const updateWrapper = (connection) => updateFn({ id: connection.id }, prepareUpdate(connection));
   const updateConnection = R.composeP(print, updateWrapper);
 
   return Promise.all(R.map(updateConnection, changes))

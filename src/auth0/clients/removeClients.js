452d8780f0e8edb6b85322f14e7e82bb13717b88
Luke van der Hoeven <hi@plukevdh.me>
2016-07-21 15:29:41 -0400

    disable removal actions, print warning instead

--- src/auth0/clients/removeClients.js
+++ src/auth0/clients/removeClients.js
@@ -1,13 +1,9 @@
 import R from 'ramda'
-import apiErrorHandler from '../../utils/apiErrorHandler'
 
 export default function removeClients(context) {
-  const { client, diff: { clients: { removes } }, say: { warn } } = context;
-  const print = ({name}) => warn("Removed client: ", name);
-  const removeFn = (obj) =>
-    client.clients.delete(R.pick(['client_id'], obj))
-      .then(_ => print(obj))
-      .catch(apiErrorHandler(obj, 'removing client', context));
+  const { diff: { clients: { removes } }, say: { warn } } = context;
+  const removeFn = ({ name, id }) =>
+    Promise.resolve(warn(`⚠️  Please remove client ${name} (${id}) manually!`));
 
   return Promise.all(R.map(removeFn, removes))
     .then(_ => context);

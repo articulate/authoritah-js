b15b27fad7dc2e3891a3f9c967182c71d163e713
Luke van der Hoeven <hi@plukevdh.me>
2016-07-22 11:39:01 -0400

    ask for confirmation before apply is run

--- src/apply/index.js
+++ src/apply/index.js
@@ -1,6 +1,7 @@
 import R from 'ramda'
 import loadEnv from '../utils/loadEnv'
 import say from '../utils/say'
+import inquirer from 'inquirer'
 
 import loadManifest from '../apply/loadManifest'
 import setupClient from '../utils/setupClient'
@@ -13,11 +14,28 @@ import diff from './diff'
 import applyDiff from './applyDiff'
 import printDiff from './printDiff'
 
+const ensure = (context) => {
+  const { config, options: { env, dryRun } } = context;
+
+  if(dryRun) { return context; }
+
+  const usingEnv = config.orGet("auth0.env", env);
+  return inquirer.prompt({
+    type: 'confirm',
+    name: 'execute',
+    default: false,
+    message: `You are about to run on the ${usingEnv} environment. Are you sure you want to do this?`,
+  }).then(R.ifElse(R.propEq('execute', true),
+    R.always(context),
+    process.exit));
+};
+
 const isDryRun = R.pathEq(['options', 'dryRun'], true);
 export default function index(filename='./auth0.yml', options) {
   const { error } = say(options);
 
   return loadEnv(options)
+    .then(ensure)
     .then(loadManifest(filename))
     .then(setupClient)
     .then(fetchRules)

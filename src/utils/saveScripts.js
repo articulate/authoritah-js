33ec62b4f8448a9fe31ec288e977f1205d54e74b
Luke van der Hoeven <hi@plukevdh.me>
2016-10-31 11:36:33 -0400

    only save src dirs if file exists

--- src/utils/saveScripts.js
+++ src/utils/saveScripts.js
@@ -6,11 +6,11 @@ const scriptName = (name) => { return name.toLowerCase().replace(/\s/g, "_") }
 // Constructs a function that saves a given script definition
 // (form of `{ script, name }`) to a file
 export default function saveScripts(dir) {
-  fs.ensureDirSync(dir);
-
   return function(scriptDefn) {
     const { script, name } = scriptDefn;
     const path = `${dir}/${scriptName(name)}.js`;
+
+    if(script) { fs.ensureDirSync(dir); }
 
     fs.writeFileSync(path, script);
     return R.assoc('script', path, scriptDefn);
